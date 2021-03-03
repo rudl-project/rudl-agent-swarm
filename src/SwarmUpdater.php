f71bd498f83b8f00a99798868c618ebf99a08286
Matthes <matthes@leuffen.de>
2021-03-03 11:24:13 +0100

    build

--- src/SwarmUpdater.php
+++ src/SwarmUpdater.php
@@ -49,8 +49,6 @@ class SwarmUpdater
 
     public function __invoke()
     {
-
-
         $swarmObj = $this->gitDb->listObjects(SWARM_SCOPE)->getObject(SWARM_CONF_OBJECT)?->hydrate(T_SwarmObj::class);
 
         if ($swarmObj === null)
